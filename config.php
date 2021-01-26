dbc15c9e0507747530e1e39394ce523c5163b0af
Fleurine Kenley <fleurinekenley@gmail.com>
2021-01-26 13:48:55 -0500

    connect to website with google account (2)

--- config.php
+++ config.php
@@ -17,7 +17,7 @@ $google_client->setClientId('233540080547-somb1ai32mrhqp8pisggl6gc3mb6m7ka.apps.
 $google_client->setClientSecret('YfTlIEL7CcZOIXDaO_y2Irft');
 
 // Définir l'URI de redirection OAuth 2.0
-$google_client-> setRedirectUri ('http://localhost/googleconnect/');
+$google_client-> setRedirectUri ('https://www.kwenpam.com/');
 
 //pour obtenir l'e-mail et le profil 
 $google_client->addScope('email');
